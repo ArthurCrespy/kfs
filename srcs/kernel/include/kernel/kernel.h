915456727a378418e65241912ffa7502cdb1daa1
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-07 12:28:17 +0100

    Main files refactoring

--- srcs/kernel/include/kernel/kernel.h
+++ srcs/kernel/include/kernel/kernel.h
@@ -2,6 +2,11 @@
 #define _KERNEL_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 void terminal_initialize(void);
 void terminal_putchar(char c);
