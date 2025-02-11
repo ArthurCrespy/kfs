0d9575e68791e62f7806b03bef48671f0163040f
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-11 12:28:09 +0100

    Fix keyboard.asm

--- srcs/kernel/kernel/IO/keyboard.asm
+++ srcs/kernel/kernel/IO/keyboard.asm
@@ -7,9 +7,7 @@ keyboard_i86_irq_asm:
 	call keyboard_i86_irq		; Call the C handler
 	popa						; Restore registers
 
-								; Send EOI to the PIC
-	mov dx, 0x20				; PIC command port
 	mov al, 0x20				; EOI command
-	out dx, al
+	out 0x20, al				; Send to PIC command port
 
-	iretd						; Return from interrupt (32-bit iret)
+	iretd						; Return from interrupt
