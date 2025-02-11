1253db1650d2942186318041109d27d38a3d65e8
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-11 11:55:52 +0100

    Typos

--- srcs/kernel/kernel/GDT/gdt.c
+++ srcs/kernel/kernel/GDT/gdt.c
@@ -1,17 +1,17 @@
 #include "../../include/kernel/gdt.h"
 
 struct gdt_entry {
-	uint16_t limit_low;		// Lower 16 bits of the segment limit.
-	uint16_t base_low;		// Lower 16 bits of the base address.
-	uint8_t  base_middle;	// Next 8 bits of the base address.
-	uint8_t  access;		// Access flags determine ring and segment type.
-	uint8_t  granularity;	// Flags and high 4 bits of limit.
-	uint8_t  base_high;		// Highest 8 bits of the base address.
+	uint16_t limit_low;		// Lower 16 bits of the segment limit
+	uint16_t base_low;		// Lower 16 bits of the base address
+	uint8_t  base_middle;	// Next 8 bits of the base address
+	uint8_t  access;		// Access flags determine ring and segment type
+	uint8_t  granularity;	// Flags and high 4 bits of limit
+	uint8_t  base_high;		// Highest 8 bits of the base address
 } __attribute__((packed));
 
 struct gdt_ptr {
-	uint16_t limit;			// Limit (size of GDT - 1).
-	uint32_t base;			// Base address of the GDT.
+	uint16_t limit;			// Limit (size of GDT - 1)
+	uint32_t base;			// Base address of the GDT
 } __attribute__((packed));
 
 // GDT array declared in assembly
@@ -27,7 +27,7 @@ void setentry(int index, uint32_t base, uint32_t limit, uint8_t access, uint8_t 
 }
 
 void gdt_init(void) {
-	setentry(0, 0, 0, 0, 0);				// 0: Null segment
+	setentry(0, 0, 0, 0, 0);			// 0: Null segment
 	setentry(1, 0, 0xFFFFF, 0x9A, 0xCF);	// 1: Kernel code segment
 	setentry(2, 0, 0xFFFFF, 0x92, 0xCF);	// 2: Kernel data segment
 	gdt_load();
--- srcs/kernel/kernel/IDT/idt.c
+++ srcs/kernel/kernel/IDT/idt.c
@@ -1,11 +1,16 @@
 #include "../../include/kernel/idt.h"
 
 struct idt_entry {
-	uint16_t offset_low;	// Lower 16 bits of the handler address.
-	uint16_t selector;		// Kernel code segment selector.
-	uint8_t  zero;			// Must be zero.
+	uint16_t offset_low;	// Lower 16 bits of the handler address
+	uint16_t selector;		// Kernel code segment selector
+	uint8_t  zero;			// Must be zero
 	uint8_t  type_attr;		// Type and attributes
-	uint16_t offset_high;	// Upper 16 bits of the handler address.
+	uint16_t offset_high;	// Upper 16 bits of the handler address
+} __attribute__((packed));
+
+struct idt_ptr {
+	uint16_t limit;			// Limit (size of IDT - 1)
+	uint32_t base;			// Base address of the IDT
 } __attribute__((packed));
 
 // IDT array declared in assembly
