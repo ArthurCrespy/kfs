6972d17d4ea3dbafaca3aec0e9638d6fc90ba33f
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-11 11:01:25 +0100

    Flag in/out functions as extern

--- srcs/kernel/include/kernel/ports.h
+++ srcs/kernel/include/kernel/ports.h
@@ -1,13 +1,12 @@
 #ifndef _PORTS_H
 #define _PORTS_H
 
-#include <stddef.h>
 #include <stdint.h>
 
-uint8_t	inb(uint16_t port);
-uint16_t inw(uint16_t port);
+extern uint8_t inb(uint16_t port);
+extern void outb(uint16_t port, uint8_t data);
 
-void outb(uint16_t port, uint8_t data);
-void outw(uint16_t port, uint16_t data);
+extern uint16_t inw(uint16_t port);
+extern void outw(uint16_t port, uint16_t data);
 
 #endif
