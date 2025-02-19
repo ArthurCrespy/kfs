5ed3beb5a7c98e1ca3c5b8258f1d3f6f5d211594
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-19 20:41:38 +0100

    Clean tests.h header

--- srcs/kernel/include/kernel/tests.h
+++ srcs/kernel/include/kernel/tests.h
@@ -1,14 +1,4 @@
 #ifndef _TESTS_H
 #define _TESTS_H
 
-#include <idt.h>
-#include <ports.h>
-
-#include <stdio.h>
-
-extern void test_irq_49_asm();
-
-void test_irq_49(void);
-void test_irq_32(void);
-
 #endif
