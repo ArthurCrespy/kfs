cbfdb1e980fe9903cc86380e584b479c720a43d0
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-19 20:41:26 +0100

    Make io_wait callable

--- srcs/kernel/include/kernel/ports.h
+++ srcs/kernel/include/kernel/ports.h
@@ -7,5 +7,6 @@ extern uint8_t inb(uint16_t port);
 extern uint16_t inw(uint16_t port);
 extern void outb(uint16_t port, uint8_t data);
 extern void outw(uint16_t port, uint16_t data);
+extern void io_wait(void);
 
 #endif
