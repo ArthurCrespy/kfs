463952a48bd403cf6c5ab83b13853cca5c812bc9
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-02-19 20:41:10 +0100

    Missing includes

--- srcs/kernel/include/kernel/kernel.h
+++ srcs/kernel/include/kernel/kernel.h
@@ -1,14 +1,15 @@
 #ifndef _KERNEL_H
 #define _KERNEL_H
 
-#include <stdio.h>
-
 #include <gdt.h>
 #include <idt.h>
+#include <irq.h>
 #include <keyboard.h>
 #include <pic.h>
 #include <terminal.h>
 #include <vga.h>
+
+#include <stdio.h>
 
 extern void interrupts_init(void);
 
